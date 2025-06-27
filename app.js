cd360e4cbda8634ebab6bc9738c7ec726dd6c984
SuMiT <sumitsahu1304@gmail.com>
2025-06-28 01:57:03 +0530

    added

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ const User = require("./models/user.js")
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js")
 const userRouter = require("./routes/user.js")
- const dbUrl = "mongodb://127.0.0.1:27017/wanderlust";
-//const dbUrl = process.env.ATLASDB_URL;
+//const dbUrl = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
     .then(()=>{
@@ -39,6 +39,7 @@ main()
 async function main(){
     // await mongoose.connect(MONGO_URL);
     await mongoose.connect(dbUrl)
+    console.log("connedted to db", dburl);
 }
 
 app.set("view engine","ejs");
